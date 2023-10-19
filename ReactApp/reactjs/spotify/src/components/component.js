3877c8215dfe56baeb7ab8f20987f9ea11753362
Bilal01-stack <gheyas842@gmai.com>
2023-10-19 22:38:32 +0500

    Update thecode

--- ReactApp/reactjs/spotify/src/components/component.js
+++ ReactApp/reactjs/spotify/src/components/component.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
 export default function Component() {
-  const [text, setText] = useState("Wlcome");
+  const [text, setText] = useState("");
+  const [color, setColor] = useState("black");
+
   const upperCase=()=>{
     let upDate = text.toUpperCase();
     setText(upDate);
@@ -9,14 +11,20 @@ export default function Component() {
   const handleClick=(event)=>{
     setText(event.target.value);
   }
+  const changeColor=()=>{
+    setColor("red");
+  }
   return (
     <>
-      <input type="text" value={text}  onChange={handleClick} placeholder="Your first name" />
+      <input type="text" value={text} style={{ color: color }}  onChange={handleClick} placeholder="Your first name" />
       <button onClick={upperCase}>Uppercase</button>
+      <button onClick={changeColor}>Change Color</button>
       <div>
         <h1>Text summay</h1>
-        <p>{text.split(" ").length} words, {text.length} character</p>
+        <p>{text.split(" ").length}  words, {text.length} character</p>
         <p>this text wil read in {text.split(" ").length/125} munits</p>
+        <h1>Preview</h1>
+        <p style={{ color: color }}>{text}</p>
       </div>
     </>
   );
