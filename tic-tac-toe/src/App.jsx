728f9f16e6c18d0458437e0db24fe47b45a50356
Bilal01-stack <gheyas842@gmai.com>
2023-10-31 19:02:02 +0500

    update the tic-tac-app

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,8 +1,23 @@
-
+import GameBoard from "./componensts/GameBoard";
+import Player from "./componensts/Player";
+import { useState } from "react";
 function App() {
+const [activePlayer, setActivePlayer] = useState()
+  function handleSelectSquare(){
+    setActivePlayer((currActivePlayer)=> currActivePlayer === 'X' ? 'O' : 'X')
+  }
   return (
-    <p>Comming Soon .....</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol= "X" />
+          <Player initialName="Player 2" symbol= "O" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
