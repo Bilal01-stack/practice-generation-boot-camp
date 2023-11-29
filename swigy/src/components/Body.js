7ba4dbabcf42f6fd977dcf68ef1113177d244f24
Bilal01-stack <gheyas842@gmai.com>
2023-11-29 12:09:22 +0500

    Add the restaurant page

--- swigy/src/components/Body.js
+++ swigy/src/components/Body.js
@@ -1,18 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestauantCard from "./Restaurant";
-import { restaurantList } from "../constants";
+//import { restaurantList } from "../constants";
+import Shimmer from "./Shimmer";
 
 export default function Body() {
   const [search, setSearch] = useState('');
-  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([])
 
-  function result(search, restaurants) {
-    const resultData =  restaurants.filter((restaurant) => 
-      restaurant.data.name.includes(search));
+  function result(search, allRestaurants) {
+    const resultData =  allRestaurants.filter((restaurant) => 
+      restaurant?.data?.name?.toLowerCase()?.includes(search.toLowerCase()));
       return resultData
   }
   
-  return (
+  useEffect(()=>{
+    callAPi();
+  }, []);
+
+
+async function callAPi(){
+   try{
+     const response = await fetch("https://www.swiggy.com/api/seo/getListing?lat=11.352407603698884&lng=75.87227602079383");
+     const json = await response.json()
+     if(json.data && json.data.success){
+      const cards = json.data.success.cards;
+      //console.log(cards)
+      const restaurants = cards[1].card.card.gridElements.infoWithStyle.restaurants;
+      //console.log(restaurants)
+      const replaceDummy = restaurants.map((res)=> {
+      return {
+        data: {
+          name: res.info?.name || "Default Name",
+          cloudinaryImageId: res.info?.cloudinaryImageId || "default-image-id",
+          locality: res.info?.locality ||  "default Location",
+          cuisines: res.info?.cuisines ||  "Cusines is not available",
+          id: res.info?.id || "Default id",
+
+        }
+      };
+     });
+     setAllRestaurants(replaceDummy);
+     setFilteredRestaurants(replaceDummy);
+
+     } else {
+      console.error("Error in API response");
+    }
+   }catch(error){
+
+   }
+}
+ 
+
+if(!allRestaurants) return null;
+  return (filteredRestaurants.length === 0)? <Shimmer/> : (
     <>
       <div>
         <input
@@ -24,18 +65,20 @@ export default function Body() {
         <button
           className="bg-green-300 ml-4 p-1 rounded"
           onClick={() => {
-            const data = result(search, restaurants);
-            setRestaurants(data);
+            const data = result(search, allRestaurants);
+            setFilteredRestaurants(data);
           }}
         >
           Search
         </button>
+        
       </div>
 
       <div className="flex gap-x-4 gap-y-4 flex-wrap">
-        {restaurants.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <RestauantCard {...restaurant.data} key={restaurant.data.id} />
         ))}
+      
       </div>
     </>
   );
--- swigy/src/components/Restaurant.js
+++ swigy/src/components/Restaurant.js
@@ -1,12 +1,13 @@
 import {IMG_CDN_URL} from "../constants";
 const RestauantCard =({ name,
-    cuisines,
+    locality,cuisines,
     cloudinaryImageId})=>{
     return(
 <div className='w-1/4 p-5 font-medium text-lg shadow-md'>
         <img className='rounded-md ' alt='' src={IMG_CDN_URL +  cloudinaryImageId} />
         <h1 className='text-3xl font-medium pt-3'>{name}</h1>
-        <span className='font-normal'>{cuisines.join(", ")}</span>
+        <h1 className='text-xl font-medium pt-3'>Location:{locality}</h1>
+        <span className='font-normal'>Cusines:  {cuisines.join(", ")}</span>
     </div>
     )
 }
